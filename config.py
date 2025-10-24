24e5b8b15e4d4984f8112d11acc0bab97c9346f7
user <1@1.com>
2025-10-24 22:34:08 +0800

    修改了FLOW_URL

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-FLOW_URL = "1.94.137.200:5001"  # 华为云转流服务
+# FLOW_URL = "https://jrlyy.fusionfintrade.com:39100"  # 华为云转流服务
+
+FLOW_URL = "http://1.94.137.200:5001"  # 华为云转流服务
+# FLOW_URL = "http://10.30.3.178:5001"  # 公司转流服务
 
 DATA_FILE = "data"
 STORAGE_JSON_FILE = "data/stream_map.json"
